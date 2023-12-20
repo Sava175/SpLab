75a5f468c59a92844a86b3f096c69ef12eee7c91
Sava175 <as1758552@gmail.com>
2023-12-20 15:21:12 +0100

    created strategy pattern

--- src/main/java/com/splab/SpLabApplication.java
+++ src/main/java/com/splab/SpLabApplication.java
@@ -5,10 +5,15 @@ package com.splab;
 import com.splab.adapter.NewUser;
 import com.splab.adapter.OldUser;
 import com.splab.adapter.UserAdapter;
+import com.splab.strategy.BubbleSort;
+import com.splab.strategy.SelectionSort;
+import com.splab.strategy.Sorter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpLabApplication {
@@ -65,6 +70,58 @@ public class SpLabApplication {
 //            System.out.println("User Name: " + adapter.getUserName());
 //            System.out.println("Age: " + adapter.getAge());
 //            ------adapter
+
+
+
+
+
+
+//            int[] numbers = {34,6,72,3,49,67,0,98,98,6,98,34};
+//            Sorter sorter = new Sorter();
+//            sorter.setSortingStrategy(new BubbleSort());
+//            sorter.performSort(numbers);
+//            System.out.println(Arrays.toString(numbers));
+//
+//            int[] otherNumbers = {34,6,72,3,49,67,0,98,98,6,98,34};
+//            sorter.setSortingStrategy(new SelectionSort());
+//            sorter.performSort(otherNumbers);
+//            System.out.println(Arrays.toString(otherNumbers));
+//
+//            -----------strategy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         };
---
+++ src/main/java/com/splab/strategy/SortingStrategy.java
@@ -0,0 +1,5 @@
+package com.splab.strategy;
+
+public interface SortingStrategy {
+    void sort(int[] array);
+}
