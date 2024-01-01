1305860b6fee2e843e830c26fa764546dc5cfd47
Sava175 <as1758552@gmail.com>
2024-01-01 14:55:50 +0100

    wrote task nint two

--- src/main/java/com/splab/tasks/NineOne.java
+++ src/main/java/com/splab/tasks/NineOne.java
@@ -1,14 +1,14 @@
 package com.splab.tasks;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import lombok.Data;
 
+import java.util.*;
+
+@Data
 public class NineOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> numbers = new ArrayList<>();
+        LinkedList<String> numbers = new LinkedList<>();
         System.out.println("enter the command/number");
 
         while (true){
@@ -27,5 +27,6 @@ public class NineOne {
                 System.out.println("the number was successfully added. What next?");
             }
         }
+
     }
 }
--- src/main/java/com/splab/tasks/NineTwo.java
+++ src/main/java/com/splab/tasks/NineTwo.java
@@ -4,9 +4,55 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class NineTwo {
+    public static final String LIST_COMMAND = "1";
+    public static final String STOP_COMMAND = "2";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        LinkedList<String> numbers = new LinkedList<>();
-        System.out.println("enter the command/number");
+        LinkedList<String> carNumbers = new LinkedList<>();
+
+        System.out.println("""
+                Enter a digit, where
+                1 = Show autos list
+                2 = Log out
+                or number of a car and number in list to add, like:
+                BH2753HO 7""");
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals(LIST_COMMAND)) {
+                showList(carNumbers);
+            } else if (command.equals(STOP_COMMAND)) {
+                System.out.println("Log out");
+                break;
+            } else {
+                addCarNumber(carNumbers, command);
+                System.out.println("Car number was successfully added. " +
+                        "Enter a digit (1 = Show autos list, 2 = Log out, or number of a car and number in list to add)");
+            }
+        }
+    }
+
+    private static void showList(LinkedList<String> carNumbers) {
+        System.out.println("Autos list:");
+        for (String number : carNumbers) {
+            System.out.println(number);
+        }
+    }
+
+    private static void addCarNumber(LinkedList<String> carNumbers, String command) {
+        String[] parts = command.split(" ");
+
+                String carNumber = parts[0];
+                int position = Integer.parseInt(parts[1]);
+
+                if (position <= 0 || position > carNumbers.size() + 1) {
+                    carNumbers.add(carNumber);
+                } else {
+                    carNumbers.add(position - 1, carNumber);
+                }
     }
 }
+
+
